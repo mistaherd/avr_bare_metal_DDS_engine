41f47a94e5af699d2ebbc1330461bd0382fafe39
mistaherd <lanzbulldog22@gmail.com>
2025-12-23 20:13:27 +0000

    following the programing manual more

--- LSFR.c
+++ LSFR.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
-int32_t LFSR(int32_t reg,int32_t N){
-  int32_t taps=0xB8;
-  for(int i=0;i<N;i++){
+int8_t LFSR(int32_t reg,int32_t N){
+  int8_t taps=0xB8;
+  for(int8_t i=0;i<N;i++){
     printf("reg: %d\n",reg);
-    int32_t res=taps&reg;
-    int32_t feedback_bit=((0b1<<7)&res)^((0b1<<5)&res)^((0b1<<4)&res)^((0b1<<3)&res)^((0b1<<0)&res);
+    int8_t res=taps&reg;
+    int8_t feedback_bit=((0b1<<7)&res)^((0b1<<5)&res)^((0b1<<4)&res)^((0b1<<3)&res)^((0b1<<0)&res);
     reg=reg>>1;
     reg |=(feedback_bit<<7);
   
@@ -15,6 +15,6 @@ int32_t LFSR(int32_t reg,int32_t N){
 }
 int main(){
 
-  int32_t reg=LFSR(9,0xF);
+  int8_t reg=LFSR(9,0xF);
   return 0;
 }
--- avr.c
+++ avr.c
@@ -1,16 +1,26 @@
-#define SREG_REG  (*(volatile uint8_t*)0x005F ))// we write to  to as  value
-#define SPH_REG   (*(volatile uint8_t*)0x005E ))
-#define SPL_REG   (*(volatile uint8_t*)0x005D)
-#define EEARL_REG (*(volatile uint8_t*)0x0041)
-#define EEPROM_Data_REG      (*(volatile uint8_t*)0x0040) // use for writeing datat to be store at 0x8 per 
+#include <stdint.h>
+#define SREG_REG                (*(volatile uint8_t*)0x005F)// we write to  to as  value
+#define SPH_REG                 (*(volatile uint8_t*)0x005E)
+#define SPL_REG                 (*(volatile uint8_t*)0x005D)
+#define EEARL_REG               (*(volatile uint8_t*)0x0041)
+#define EEPROM_Data_REG         (*(volatile uint8_t*)0x0040)// use for writeing datat to be store at 0x8 per 
 #define EEPROM_Control_REG      (*(volatile uint8_t*)0x003F)
-enum modes{Erase_and_write,Erase,Write};
+#define EEPROM_Control_Read_REG (*(volatile uint8_t*)0x001F)
+#define SPMCSR_read             (*(volatile uint8_t*)0x0037)
+#define GPIOR2                  (*(volatile uint8_t*)0x004B)
+#define GPIOR1                  (*(volatile uint8_t*)0x004A)
+#define GPIOR0                  (*(volatile uint8_t*)0x003E)
+#define CLKPR                   (*(volatile uint8_t*)0x0061)
 
-int EEPROM_MODE(modes mode ,uint8_t EEPROM_Control_REG_value){
-   switch (mode){
-    case Erase_and_write:
-      EEPROM_Control_REG_value &= ~0x30;
-      break;
+
+
+
+#define Clock_Frequency  8000000
+typedef enum {Erase_and_Write,Erase,Write} modes_t;
+
+uint8_t SET_EEPROM_MODE(modes_t mode ,uint8_t EEPROM_Control_REG_value){
+  switch (mode){
+    case Erase_and_Write:break;
     case Erase:
       EEPROM_Control_REG_value |= 0x10;
       break;
@@ -18,22 +28,59 @@ int EEPROM_MODE(modes mode ,uint8_t EEPROM_Control_REG_value){
       EEPROM_Control_REG_value |= 0x20;
       break;
     default:
-      perror("Error while writeing EEPROM Modes");
+      return -1;
       break;
   }
+  return EEPROM_Control_REG_value;
 }
-void avr_driver_init(){
+void WAIT_AMOUNT_OF_CLOCK_CYCLES(int8_t Amount_of_clock_cycles){
+  // caluute the clock Frequnecy 1/F = the amount time for 1 cycle
+  while(Amount_of_clock_cycles--){
+    __asm__ volatile ("nop");
+  }
+}
+int8_t  EEPROM_WRITE_ENABLE(){
+  // consider a while but it  be nice  to not have to  have in a loop
+  // Wait until EEPE becomes zero.
+  if (EEPROM_Control_Read_REG&0x2==0x0){
+    // Wait until SELFPRGEN in SPMCSR becomes zero.
+    if (SPMCSR_read&0x0==0){
+      // Write a logical one to the EEMPE bit while writing a zero to EEPE in EECR.
+      EEPROM_Control_REG=0x6;
+
+      // Within four clock cycles after setting EEMPE, write a logical one to EEPE.
+      Wait_Amount_of_clock_cycles(4);
+      EEPROM_Control_REG =0x2;
+      return 0;
+    }
+
+  }
+  else {
+    return -1;    
+    //break //in case of we write a  while loop
+  }
+}
+void EEPROM_WRITE(uint16_t uiAddress,Unsigened char ucData){
+
+  EEARL_REG=uiAddress;
+  EEPROM_Data_REG=ucData;
+}
+void AVR_DRIVER_INIT(){
   // set the global interupt enable to be on.
-  SREG_REG |= 0x80;
+  SREG_REG= 0x80;
   // set Stack pointer high to the highest 
   SPH_REG = 0xFF;
   // set Stack pointer low to the highest 
   SPL_REG =0x8;
   // set_ EEARL to 256 bytes
   EEARL_REG =0x8;
-  
+  // set the EEPROM_Control_REG_value
+  EEPROM_Control_REG=0xC;
+  EEPROM_Control_REG=SET_EEPROM_MODE(Erase_and_write,EEPROM_Control_REG);
+  EEPROM_WRITE_ENABLE();
+
 }
 int main(){
-  ardunio_driver_init();
+  AVR_DRIVER_INIT();
   return 0;
 }
